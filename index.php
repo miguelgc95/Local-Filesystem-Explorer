18f7c65425e4bd85fe697830ab0f1cd451f87e0d
miguelgc95 <miguelgc95@gmail.com>
2021-02-03 14:09:08 +0100

    Create basic structure

--- index.php
+++ index.php
@@ -1,12 +1,38 @@
+<?php
+
+        // //mkdir("./root/nuevofolder");
+        // $fichero = './root/gente.txt';
+        // // Abre el fichero para obtener el contenido existente
+        // $actual = file_get_contents($fichero);
+        // // Añade una nueva persona al fichero
+        // $actual .= "Miguel Gar\n";
+        // // Escribe el contenido al fichero
+        // file_put_contents($fichero, $actual);
+
+    ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File system manager</title>
+    <link rel="stylesheet" href="assets/css/normalize.css">
     <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
-    
+    <nav class="navbar">
+        searcher
+    </nav>
+    <section class="wrapper">
+        <aside class="wrapper-tree">
+        </aside>
+        <section class="wrapper-main">
+        </section>
+        <aside class="wrapper-info">
+        </aside>
+    </section>
+
 </body>
 </html>
