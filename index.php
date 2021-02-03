9b2854ba4c3e4c6ff1f0098fbfe07f585406bb75
miguelgc95 <miguelgc95@gmail.com>
2021-02-03 18:00:49 +0100

    Learning some php stuff

--- index.php
+++ index.php
@@ -20,6 +20,8 @@
     <title>File system manager</title>
     <link rel="stylesheet" href="assets/css/normalize.css">
     <link rel="stylesheet" href="assets/css/styles.css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0-0/axios.js" type="text/javascript"></script>
 </head>
 <body>
     <nav class="navbar">
@@ -29,10 +31,51 @@
         <aside class="wrapper-tree">
         </aside>
         <section class="wrapper-main">
+            <?php
+                $path = getcwd()."/root";
+                echo $path;
+                echo '<br>';
+                echo '<br>';
+                $folder = scandir($path);
+                echo "<pre>";
+                print_r ($folder); //array
+                echo "</pre>";
+
+                /* $dir = $path; */
+
+                // Abre un directorio conocido, y procede a leer el contenido
+                /* if (is_dir($dir)) {
+                    if ($dh = opendir($dir)) {
+                        while (($file = readdir($dh)) !== false) {
+                            echo "nombre archivo: $file : tipo archivo: " . filetype($dir . $file) . "<br>";
+                        }
+                        closedir($dh);
+                    }
+                } */
+
+                $foldercount = count($folder);
+                /* echo $foldercount; */
+                for ($i=0; $i < $foldercount ; $i++) {
+                    if ($folder[$i] != '.' && $folder[$i] != '..') {
+                        /* print_r($folder[$i]); */
+                        echo '<a class="nameFolder" href=./root/' . $folder[$i].'>' . '<h2>' . $folder[$i] .  '</h2>' . '</a>';
+                        //echo '<a class="nameFolder" href='. $path . '/' . $folder[$i] . '>' . '<h2>' . $folder[$i] .  '</h2>' . '</a>';
+                        //echo '<p>' . $path . '/' . $folder[$i] . '</p>';
+                    }
+                }
+
+            ?>
         </section>
         <aside class="wrapper-info">
         </aside>
     </section>
 
+    <script>
+        axios.get(url)
+        .then()
+        .catch(error => {
+        console.log("error", error);
+    })
+    </script>
 </body>
 </html>
