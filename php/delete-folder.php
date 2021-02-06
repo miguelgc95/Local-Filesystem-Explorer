adb0c2d678ce96dfdf8e5819f6ee45de1523ada3
miguelgc95 <miguelgc95@gmail.com>
2021-02-06 13:10:20 +0100

    Deleting recursively without automatic reload

--- php/delete-folder.php
+++ php/delete-folder.php
@@ -1,4 +1,32 @@
 <?php
 
-    $semiPath = urldecode($_POST["semiPath"]);
-    rmdir("../root/$semiPath");
+$semiPath = urldecode($_POST["semiPath"]);
+
+$dir = "../root$semiPath";
+
+rrmdir($dir);
+function rrmdir($dir)
+{
+    $folder = scandir($dir);
+    var_dump($folder);
+    if (count($folder)==2){
+        echo "<br>";
+        echo "borro a: $dir";
+        echo "<br>";
+        rmdir($dir);
+    }else{
+        foreach($folder as $value){
+            if (is_file($dir."/".$value)){
+                unlink($dir."/".$value);
+            }elseif (is_dir($dir."/".$value)) {
+                if ($value!="." && $value!=".."){
+                    echo "<br>";
+                    echo "recursivo";
+                    echo "<br>";
+                    rrmdir($dir."/".$value);
+                }
+            }
+        }
+        rmdir($dir);
+    }
+}
