5cff21d0766b2cddeba495f72b6856c174c98d2a
miguelgc95 <miguelgc95@gmail.com>
2021-02-07 13:17:18 +0100

    Upload a file with automatic reload

--- php/upload.php
+++ php/upload.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Estoy llegando flama";
+$path = urldecode($_POST['path']);
+$dir = "../root$path";
+$name = $_FILES['upload']['name'];
+$parent = "/Local-Filesystem-Explorer/?dir=" . $_POST["parent"];
+
+move_uploaded_file($_FILES['upload']['tmp_name'], "$dir/$name");
+
+header('Location: '.$parent);
+die();
--- php/uploadFileButton.php
+++ php/uploadFileButton.php
@@ -1,6 +1,7 @@
 <?php
-
-echo '<form id="upload-form" method="post" action="./php/upload.php">' . "\n"
-    . '<input type="hidden" name="" value="">' . "\n"
+$parent = urlencode($aux);
+echo '<form id="upload-form" method="post" action="./php/upload.php" enctype="multipart/form-data">' . "\n"
+    . '<input type="hidden" name="path" value="'.$myquery.'">' . "\n"
+    . '<input type="hidden" name="parent" value=' . $parent . '>' . "\n"
     . '<input id="upload-file" type="file" name="upload">' . "\n"
     . '</form>';
