4760097133d87332ca00bfc9ec358d8a842f5775
miguelgc95 <miguelgc95@gmail.com>
2021-02-08 20:03:54 +0100

    Unresolved conflict

--- php/uploadFileButton.php
+++ php/uploadFileButton.php
@@ -1,10 +1,5 @@
 <?php
-<<<<<<< HEAD
 $parent = urlencode($myquery);
-=======
-
-$parent = urlencode($aux);
->>>>>>> main
 echo '<form id="upload-form" class="upload-form" method="post" action="./php/upload.php" enctype="multipart/form-data">' . "\n"
     . '<input type="hidden" name="path" value="'.$myquery.'">' . "\n"
     . '<input type="hidden" name="parent" value=' . $parent . '>' . "\n"
